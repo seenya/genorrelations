6f70a980f76668d07ee33edb14b7362a0ff463f0
Sean Fleming <seenya@gmail.com>
2013-10-27 12:16:28 +1100

    Add top genes to view

--- routes/index.js
+++ routes/index.js
@@ -12,21 +12,42 @@ exports.visualise = function(req, res){
     });
 };
 
+var composeJSON = function(geneIndex, data, metadata) {
+    var composedJSON = {};
+
+    composedJSON.name = data.name;
+    composedJSON.nodes = data.nodes;
+    composedJSON.edges = data.edges;
+    composedJSON.topGenes = metadata.topReferencedIndexes;
+
+   // Add the d3 attributes to the node names
+    composedJSON.nodes.forEach(function(node) {
+        node.label = node.name;
+        node.radius = 5;
+        node.fill = "#c2c2c2";
+        node.stroke = "#a6a6a6";
+    });
+
+    return composedJSON;
+};
+
 var getGroupJSON = function(id, geneIndex, callback) {
     var filename = './data/' + id + '.json';
+    var metaFilename = './data/' + id + '.meta.json';
     fs.readFile(filename, 'utf8', function (err, data) {
         if (err)
             callback(err);
         else {
             var jsonData = JSON.parse(data);
-            // Add the d3 attributes to the node names
-            jsonData.nodes.forEach(function(node) {
-                node.label = node.name;
-                node.radius = 5;
-                node.fill = "#c2c2c2";
-                node.stroke = "#a6a6a6";
+ 
+            fs.readFile(metaFilename, 'utf8', function (err, data) {
+                if (err)
+                    callback(err);
+                else {
+                    var metadata = JSON.parse(data);
+                    callback(null, composeJSON(geneIndex, jsonData, metadata));
+                }
             });
-            callback(null, jsonData);
         }
     });
 };
